4b953e10213fed2526a36ede03426f28e0eab487
Luan Au <luanau@gmail.com>
2017-07-13 00:17:15 +0100

    Form created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Validators, FormGroup, FormArray, FormBuilder } from '@angular/forms';
+import { Customer } from './customer.interface';
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+    moduleId: module.id,
+    selector: 'my-app',
+    templateUrl: 'app.component.html',
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent implements OnInit {
+    public myForm: FormGroup; // our form model
+
+    // we will use form builder to simplify our syntax
+    constructor(private _fb: FormBuilder) { }
+
+    save(model: Customer) {
+        // call API to save customer
+        console.log(model);
+    }
+
+    ngOnInit() {
+        // we will initialize our form here
+        this.myForm = this._fb.group({
+                name: ['', [Validators.required, Validators.minLength(5)]],
+                addresses: this._fb.array([
+                    this.initAddress(),
+                ])
+            });
+        }
+
+    initAddress() {
+            // initialize our address
+            return this._fb.group({
+                street: ['', Validators.required],
+                postcode: ['']
+            });
+        }
+
+    addAddress() {
+        // add address to the list
+        const control = <FormArray>this.myForm.controls['addresses'];
+        control.push(this.initAddress());
+    }
+
+    removeAddress(i: number) {
+        // remove address from the list
+        const control = <FormArray>this.myForm.controls['addresses'];
+        control.removeAt(i);
+    }
+}
